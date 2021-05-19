eaadb2bccf912cf399f6deefc6e5b939bb5cefd7
Matt <matt.snell.00@hotmail.co.uk>
2021-05-19 12:47:28 +0000

    home urls.py added and basic home view created with test message

--- home/views.py
+++ home/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib import messages
 
-# Create your views here.
+
+def home(request):
+    """ a view for the home page """
+    template = 'home/home.html'
+    messages.success(request, "The messages are working if you can see this")
+    context = {
+        'title': 'home',
+    }
+    return render(request, template, context)
