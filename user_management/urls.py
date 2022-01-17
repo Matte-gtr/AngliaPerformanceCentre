5afefff589c6f4c80194b6f8c4b10b208c2d7a1b
Matt <matt.snell.00@hotmail.co.uk>
2022-01-17 02:13:28 +0000

    add messages and callbacks to admin panel with very basic styling

--- user_management/urls.py
+++ user_management/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('profile/', views.profile, name="profile")
+    path('profile/', views.profile, name="profile"),
+    path('admin_panel/', views.admin_panel, name="admin_panel"),
 ]
--- user_management/views.py
+++ user_management/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from contact.models import Message, Callback
 
 
 def profile(request):
@@ -6,5 +7,21 @@ def profile(request):
     template = "user_management/profile.html"
     context = {
         'title': 'profile',
+        'section': 'user_management',
+    }
+    return render(request, template, context)
+
+
+def admin_panel(request):
+    """ a view to display the admin panel """
+    # Pagination required
+    site_messages = Message.objects.all()
+    callbacks = Callback.objects.all()
+    template = "user_management/admin_panel.html"
+    context = {
+        'title': 'admin_panel',
+        'section': 'user_management',
+        'site_messages': site_messages,
+        'callbacks': callbacks,
     }
     return render(request, template, context)
