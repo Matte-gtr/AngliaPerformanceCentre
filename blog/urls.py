8210e43308b0c1be833d06acaad0911120366b22
Matt <matt.snell.00@hotmail.co.uk>
2022-02-16 14:41:04 +0000

    add view post page with minimal layout (pre-install of django-inline-media)

--- blog/urls.py
+++ blog/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.blog, name="blog")
+    path('', views.blog, name="blog"),
+    path('view_post/<int:blog_id>/', views.view_post, name="view_post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import BlogPost, BlogCategory
 from user_management.views import paginator_helper
 
@@ -7,7 +7,7 @@ def blog(request):
     """ a view to display the blog page """
     category = "All"
     filter_cat = None
-    blog_posts = BlogPost.objects.all().order_by('added_on')
+    blog_posts = BlogPost.objects.filter(public=True).order_by('added_on')
     for post in blog_posts:
         post.short_body = post.post_body[:200]
     categories = BlogCategory.objects.all()
@@ -16,17 +16,30 @@ def blog(request):
         if 'category' in request.GET:
             filter_cat = request.GET['category']
             blog_posts = blog_posts.filter(category__category=filter_cat)
-            category = request.GET['category']
+            category = BlogCategory.objects.filter(category=filter_cat)
     
     blog_posts = paginator_helper(request, blog_posts.
                                   order_by('added_on'), 8)
+    for post in blog_posts:
+        post.short_body = post.post_body[:200]
     template = "blog/blog.html"
     context = {
         'title': 'blog',
         'section': 'blog',
         'blog_posts': blog_posts,
         'categories': categories,
-        'filter_cats': filter_cat,
         'category': category,
+    }
+    return render(request, template, context)
+
+
+def view_post(request, blog_id):
+    """a view to show a full blog post write-up"""
+    blog_post = get_object_or_404(BlogPost, pk=blog_id)
+    template = "blog/view_post.html"
+    context = {
+        'title': blog_post.post_title,
+        'section': 'blog',
+        'blog_post': blog_post,
     }
     return render(request, template, context)
