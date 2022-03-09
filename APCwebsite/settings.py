f0ace20dc4877cdf6533a27b45dd00f920bfb104
Matt <matt.snell.00@hotmail.co.uk>
2022-03-09 15:41:25 +0000

    change blog header image display, add Procfile

--- APCwebsite/settings.py
+++ APCwebsite/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,13 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-j*bm-73hpdlt-d)#i(t+b4a#y2k)grgv1s0+(p9b@9*+at8-xz'
+# DEVELOPMENT Unset
+if 'DEVELOPMENT' in os.environ:
+    SECRET_KEY = 'django-insecure-j*bm-73hpdlt-d)#i(t+b4a#y2k)grgv1s0+(p9b@9*+at8-xz'
+    DEBUG = True
+else:
+    SECRET_KEY = os.environ.get('SECRET_KEY')
+    DEBUG = False
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['109.74.202.25']
 
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
@@ -73,7 +76,7 @@ INSTALLED_APPS = [
     'contact',
     'cart',
     'user_management',
-
+    
     'django_cleanup.apps.CleanupConfig',
 ]
 
@@ -135,12 +138,17 @@ WSGI_APPLICATION = 'APCwebsite.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
@@ -202,6 +210,6 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
-    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASS')
-    DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
+    EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER') # unset
+    EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASS') # unset
+    DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL') # unset
---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn AngliaPerformanceCentre.wsgi:application
