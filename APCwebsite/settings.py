6aa3274a4742de1a5acd8a2c5dffcd4ab36c5559
Matt <matt.snell.00@hotmail.co.uk>
2022-03-16 11:54:37 +0000

    further settings update

--- APCwebsite/settings.py
+++ APCwebsite/settings.py
@@ -219,9 +219,9 @@ else:
     EMAIL_USE_TLS = True
     EMAIL_PORT = 587
     EMAIL_HOST = 'smtp.gmail.com'
-    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER_TEST')
-    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS_TEST')
-    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL_TEST')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
 
 if 'USE_AWS' in os.environ:
     # Cache control
